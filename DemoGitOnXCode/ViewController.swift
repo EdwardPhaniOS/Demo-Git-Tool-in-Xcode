5aee9a9a344df15b546c1c7626fa8ca4399827a7
EdwardPhaniOS <EdwardPhan2015@gmail.com>
2019-11-06 12:59:29 +0700

    Initial Commit

---
+++ DemoGitOnXCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGitOnXCode
+//
+//  Created by Tan Vinh Phan on 11/6/19.
+//  Copyright © 2019 PTV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
